bbdff1143638a00c6a6f5568d1a14010e71a8262
Augive2613 <1733677917@qq.com>
2023-02-15 09:26:50 +0800

    Demo 1

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void fun(int step)
+{
+
+}
+int main()
+{
+    int i;
+    double sum=1,a=1;
+    // for(i=1;i<50;i++) 
+    // {
+    //     a=a*i;
+    //     sum=sum+(1/a);
+    // }
+
+    // 50 为计算的项数
+fun(50);
+   printf("%lf",sum);
+   return 0;
+}
