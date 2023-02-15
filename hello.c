4181b4e120d6dc271b1a714831e6556d47fe5d5f
Augive2613 <125175713+Augive2613@users.noreply.github.com>
2023-02-15 15:25:22 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
-void fun(int step)
-{
-
-}
 int main()
 {
     int i;
     double sum=1,a=1;
-    // for(i=1;i<50;i++) 
-    // {
-    //     a=a*i;
-    //     sum=sum+(1/a);
-    // }
-
-    // 50 为计算的项数
-fun(50);
+    for(i=1;i<50;i++) 
+    {
+        a=a*i;
+        sum=sum+(1/a);
+    }
    printf("%lf",sum);
    return 0;
 }
