c3f1c9e43dd86c58e195af978101fc027b773325
Augive2613 <1733677917@qq.com>
2023-02-25 16:04:29 +0800

    Demo6.c

--- Demo6.c
+++ Demo6.c
@@ -1,6 +1,23 @@
-#include<stdio.h>
-typedef struct 
+#include <stdio.h>
+typedef struct
 {
-    int a;
-    char b;
-}ST;
+    double score;
+    int age;
+} stu;
+typedef struct
+{
+    double a;
+    int b;
+} note;
+
+int main()
+{
+    stu s = {78.5, 22};
+    note c = {0, 0};
+    stu *p;
+    note *q;
+    p = &s;
+    q->a = p->score;
+    q->b = p->age;
+    printf("%f %d", q->a, q->b);
+}
